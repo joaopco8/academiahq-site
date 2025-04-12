0a4e5850bf84072dc39296ccba009c08acda0f9e
joaopco8 <joaopedro.co@hotmail.com>
2025-04-12 12:08:46 -0300

    Adiciona Meta Pixel do Facebook para rastreamento

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans, GeistMono } from "geist/font";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from 'next/script';
 
 const geistSans = GeistSans;
 const geistMono = GeistMono;
@@ -19,6 +20,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className="dark">
+      {/* Meta Pixel Code */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1227385201728303');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img 
+          height="1" 
+          width="1" 
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=1227385201728303&ev=PageView&noscript=1"
+          alt=""
+        />
+      </noscript>
+      {/* End Meta Pixel Code */}
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground relative`}>
         {/* Background com tons azuis */}
         <div 
